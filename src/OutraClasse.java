48668c7feb5e00d7654a1a504596ec6d0c94d243
Daniel Ferreira <danenoisturbera@gmail.com>
2020-09-23 15:51:30 -0300

    exercicios conversados no dia 23/09/2020

--- src/OutraClasse.java
+++
@@ -1,6 +0,0 @@
-
-public class OutraClasse {
-	public static void main(String[] args) {
-		System.out.println("teste da outra classe");
-	}
-}
--- src/tipodados/Main.java
+++ src/tipodados/Main.java
@@ -1,73 +1,115 @@
 package tipodados;
 
-
+import java.lang.reflect.Array;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
 
-
-
 public class Main {
-	public static void main(String args []) {
-		
-		
-		
-		
-		
-		
+	public static void main(String args[]) {
+
 	}
-	
-	public static void ArrayComLoops() {
-		//Exemplos de array muntimensional;
-		double[][] matrix = {{1.2, 4.3, 4.0}, 
-			      {4.1, -1.1}
-			};
-		
-		String[] arrayDeString = new String[2];
-		String[] arrayDeSTringOutraForma = {"Daniel", "DAyana", "MArcinha"};			
-		
-		int[] arrayDeInteiros = new int[2];
-		
-		arrayDeString[0] = "Daniel é burrinho hein kkkk";
-		arrayDeString[1] = "Mas isso não importa";
-		
-		
-		
-		arrayDeInteiros[0] = 1;
-		arrayDeInteiros[1] = 2;
-		
-		System.out.println("Array for");
-		for (int i = 0; i < arrayDeString.length; i++) {
-			
-			System.out.println(arrayDeString[i]);
+
+	public static void exibeMedia(int[] array) {
+		int media = 0;
+		int total = 0;
+		for (int numero : array) {
+			total += numero;
+		}
+
+		media = total / array.length;
+
+		System.out.println("A média dos números do array é de: " + media);
+
+	}
+
+	public static void exibeTotal(int[] array) {
+
+		int total = 0;
+		for (int numero : array) {
+			total += numero;
+		}
+
+		System.out.println("A média dos números do array é de: " + total);
+
+	}
+
+	public static void exibeCincoPrimeiroESomaCincoUltimos(int[] array) {
+
+		int somaDosUltimos5Numeros = 0;
+		for (int i = 0; i < array.length; i++) {
+			if (i > 4) {
+				somaDosUltimos5Numeros += i;
+			} else {
+				System.out.println("Posicao " + i + 1 + " Valor:" + i);
+			}
+
 		}
 		
-		System.out.println("Array foreach");						
-		for (String nome : arrayDeString) {
-			
+		System.out.println("soma dos últimos cinco números: "+ somaDosUltimos5Numeros);
+
+	}
+	
+	public static void nomesComAsteriscos(String [] nomes) {
+		
+		for (String nome : nomes) {
+			System.out.println("**" + nome + "**");
+		}
+	}
+	
+	public static void nomesEmOrdemAlfabetica(String [] nomes) {
+		Arrays.sort(nomes);
+		
+		for (String nome : nomes) {
 			System.out.println(nome);
 		}
 		
-		
-		
+	}
+
+	public static void ArrayComLoops() {
+		// Exemplos de array muntimensional;
+		double[][] matrix = { { 1.2, 4.3, 4.0 }, { 4.1, -1.1 } };
+
+		String[] arrayDeString = new String[2];
+		String[] arrayDeSTringOutraForma = { "Daniel", "DAyana", "MArcinha" };
+
+		int[] arrayDeInteiros = new int[2];
+
+		arrayDeString[0] = "Daniel é burrinho hein kkkk";
+		arrayDeString[1] = "Mas isso não importa";
+
+		arrayDeInteiros[0] = 1;
+		arrayDeInteiros[1] = 2;
+
+		System.out.println("Array for");
+		for (int i = 0; i < arrayDeString.length; i++) {
+
+			System.out.println(arrayDeString[i]);
+		}
+
+		System.out.println("Array foreach");
+		for (String nome : arrayDeString) {
+
+			System.out.println(nome);
+		}
+
 		int i = 0;
-		while(i < arrayDeString.length) {
+		while (i < arrayDeString.length) {
 			System.out.println(arrayDeString[i]);
 			i++;
 		}
-		
+
 		int indice = 0;
 		do {
 			System.out.println(arrayDeString[i]);
 			indice++;
 		} while (indice < arrayDeString.length);
-		
+
 	}
-	
-	
-	
+
 	public static void ExibirTiposdeDados() {
-		
+
 		int numero = 1;
 		boolean isItau = false;
 		byte numeroByte = 2;
@@ -79,7 +121,7 @@ public class Main {
 		double numeroFlutuanteDouble = 42.3;
 		char letra = 'a';
 		String palavra = "word";
-							
+
 		System.out.println(numero);
 		System.out.println(isItau);
 		System.out.println(numeroByte);
@@ -91,71 +133,68 @@ public class Main {
 		System.out.println(numeroFlutuanteDouble);
 		System.out.println(letra);
 		System.out.println(palavra);
-		
+
 	}
-	
+
 	public static void UsandoOperadoresAritmeticos() {
-		
+
 		double number1 = 12.5, number2 = 3.5, result;
-    	
-        // Using addition operator
-        result = number1 + number2;
-        System.out.println("number1 + number2 = " + result);
-    	
-        // Using subtraction operator
-        result = number1 - number2;
-        System.out.println("number1 - number2 = " + result);
-    	
-        // Using multiplication operator
-        result = number1 * number2;
-        System.out.println("number1 * number2 = " + result);
 
-        // Using division operator
-        result = number1 / number2;
-        System.out.println("number1 / number2 = " + result);
-    	
-        // Using remainder operator
-        result = number1 % number2;
-        System.out.println("number1 % number2 = " + result);
-		
-	} 
-	
-	
-	public static void Concatenacao() {
-		 String start, middle, end, result;
-	    	
-	        start = "Talk is cheap. ";
-	        middle = "Show me the code. ";
-	        end = "- Linus Torvalds";
-	    	
-	        result = start + middle + end;
-	        System.out.println(result);
-		
+		// Using addition operator
+		result = number1 + number2;
+		System.out.println("number1 + number2 = " + result);
+
+		// Using subtraction operator
+		result = number1 - number2;
+		System.out.println("number1 - number2 = " + result);
+
+		// Using multiplication operator
+		result = number1 * number2;
+		System.out.println("number1 * number2 = " + result);
+
+		// Using division operator
+		result = number1 / number2;
+		System.out.println("number1 / number2 = " + result);
+
+		// Using remainder operator
+		result = number1 % number2;
+		System.out.println("number1 % number2 = " + result);
+
 	}
-	
-	
-	 public static void OperadoresUnario() {
-	    	
-	        double number = 5.2, resultNumber;
-	        boolean flag = false;
-	    	
-	        System.out.println("+number = " + +number);
-	        // number is equal to 5.2 here.
-	        
-	        System.out.println("-number = " + -number);
-	        // number is equal to 5.2 here.
-	    	
-	        // ++number is equivalent to number = number + 1
-	        System.out.println("number = " + ++number);
-	        // number is equal to 6.2 here.
 
-	        // -- number is equivalent to number = number - 1
-	        System.out.println("number = " + --number);
-	        // number is equal to 5.2 here.
+	public static void Concatenacao() {
+		String start, middle, end, result;
 
-	        System.out.println("!flag = " + !flag);
-	        // flag is still false.
-	    }
-	
-	
+		start = "Talk is cheap. ";
+		middle = "Show me the code. ";
+		end = "- Linus Torvalds";
+
+		result = start + middle + end;
+		System.out.println(result);
+
+	}
+
+	public static void OperadoresUnario() {
+
+		double number = 5.2, resultNumber;
+		boolean flag = false;
+
+		System.out.println("+number = " + +number);
+		// number is equal to 5.2 here.
+
+		System.out.println("-number = " + -number);
+		// number is equal to 5.2 here.
+
+		// ++number is equivalent to number = number + 1
+		System.out.println("number = " + ++number);
+		// number is equal to 6.2 here.
+
+		// -- number is equivalent to number = number - 1
+		System.out.println("number = " + --number);
+		// number is equal to 5.2 here.
+
+		System.out.println("!flag = " + !flag);
+		// flag is still false.
+	}
+
 }
